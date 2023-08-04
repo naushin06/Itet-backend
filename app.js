1524548ca626f0c3e37e759807dd25da4cd9094a
naushin <nnaushin2002@gmail.com>
2023-08-04 16:45:34 +0530

    Deploying changes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express =require("express");
 const app=express();
 const cors=require('cors');
 const mongoose=require("mongoose")
+const port = process.env.PORT || 3001
 
 app.use(cors());
 app.use(express.json())
@@ -10,7 +11,7 @@ mongoose.connect("mongodb+srv://root:password123*@cluster0.hxeftjt.mongodb.net/I
     console.log("db connected");
 })
 
-app.listen("/",function(){
+app.listen(port,function(){
     console.log("app started to run on 3001")
 })
 app.use('/',require("./Routes/userRoutes"))
