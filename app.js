4e5bb27b815314ae5c093723ed913ef099a0b95f
naushin <nnaushin2002@gmail.com>
2023-08-04 20:31:21 +0530

    changing mongoose cluster to itet

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json())
 
-mongoose.connect("mongodb+srv://root:password123*@cluster0.hxeftjt.mongodb.net/ITET-EVENTMANAGMENT").then(function(){
+mongoose.connect("mongodb+srv://itexperttraining:itExpertraining_2023@cluster0.y1ctxvj.mongodb.net/ITET-EVENTMANAGMENT").then(function(){
     console.log("db connected");
 })
 
-app.listen(port,function(){
+app.listen(3001,function(){
     console.log("app started to run on 3001")
 })
 app.use('/',require("./Routes/userRoutes"))
