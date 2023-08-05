73909b49ac01365963a3c7a26143aa958ab528fd
naushin <nnaushin2002@gmail.com>
2023-08-05 20:21:59 +0530

    removing cors

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const app=express();
 const cors=require('cors');
 const mongoose=require("mongoose")
 const port = process.env.PORT || 5000
-const corsOptions = {
-    origin: "https://itet-frontend.vercel.app/", // Replace with your frontend's domain
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true
-  };
+// const corsOptions = {
+//     origin: "https://itet-frontend.vercel.app/", // Replace with your frontend's domain
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     credentials: true
+//   };
   
-  app.use(cors(corsOptions));
+//   app.use(cors(corsOptions));
   
-// app.use(cors());
+app.use(cors());
 app.use(express.json())
 
 mongoose.connect("mongodb+srv://itexperttraining:itExpertraining_2023@cluster0.y1ctxvj.mongodb.net/ITET-EVENTMANAGMENT").then(function(){
