5e1e1b63906916435f848a6cbd50fd258487d0dd
naushin06 <89929366+naushin06@users.noreply.github.com>
2023-08-31 18:38:42 +0530

    Update userRoutes.js

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -10,6 +10,7 @@ router.route("/create").post((req,res) =>{
    const ttime=req.body.ttime
    const tname=req.body.tname;
    const desc=req.body.desc ;
+   const mode:req.body.mode
     const newEvent=new Event({
         name,
        fdate,
@@ -18,6 +19,7 @@ router.route("/create").post((req,res) =>{
        ttime,
         tname,
         desc,
+         mode 
     })
 newEvent.save();
 res.send("the task is completed")
